4d321c94d5ba019152bbe7c8ac88b1a01316e090
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2023-12-17 09:54:59 -0300

    att
    att

---
+++ aula11/ex007.js
@@ -0,0 +1,7 @@
+var teste = 55.21
+
+if (teste >= 60){ /* Condição composta */
+    console.log('Você ultrapassou a velocidade permitida.')
+} else {
+    console.log('Você está dentro da velocidade permitida.')
+}
---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var país = 'Inglaterra'
+
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro.')
+} else {
+    console.log('Você é estrangeiro. Hi my brotha, Hola que tal, Orrayo, Bonjour.')
+}
