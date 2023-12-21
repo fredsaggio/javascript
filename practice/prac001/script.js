3cf5eb49d7d939c63569d9d88a83b76929b896b0
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2023-12-20 21:49:09 -0300

    Atividade criada do meu jeito

--- practice/prac001/script.js
+++ practice/prac001/script.js
@@ -10,19 +10,19 @@ function carregar(){
     if (teste >= 4 && teste < 12) {
 
         horacerta.innerHTML += `<p>Agora é de manhã, sinistro meu cria!</p>`;
-        imagemhorario.src = '../imagens/fotomanha.jpg';
+        imagemhorario.src = '../imagens/homem-criança.jpg';
         document.body.style.background = 'yellow';
 
     } else if (teste >= 12 && teste < 18) {
 
         horacerta.innerHTML += `<p>Agora é de <strong>tarde</strong>, vambora filho!</p>`;
-        imagemhorario.src = '../imagens/fototarde.jpg';
+        imagemhorario.src = '../imagens/mulher-jovem.jpg';
         document.body.style.background = 'green';
 
     } else { 
 
         horacerta.innerHTML += `<p>Agora é de <strong>noite</strong>, sinistro jow</p>`;
-        imagemhorario.src = '../imagens/fotonoite.jpg';
+        imagemhorario.src = '../imagens/mulher-idosa.jpg';
         document.body.style.background = 'red';
     }
 
--- practice/prac002/script.js
+++ practice/prac002/script.js
@@ -1,3 +1,55 @@
-function verificar() {
+function verificar() { 
+    var data = new Date();
+    var anoatual = data.getFullYear();
+
+    var caixanumber = document.getElementById('itxtano');
+    var res = document.getElementById('container2');
+    var idade = anoatual - caixanumber.value
     
+    var genero = ''
+    var fsex = document.getElementsByName('radsex')
+
+    var imagem = document.getElementById('imagem')
+
+    res.innerHTML = `Sua idade é igual a ${idade}`
+
+
+    if (caixanumber.value.length == 0 || Number(caixanumber.value) > anoatual) {
+        res.innerHTML = `<p>Idade <ins>inválida</ins></p>`
+} else {
+    if (fsex[0].checked) {
+
+        genero = 'Homem'
+
+        if (idade >= 0 && idade < 10) {
+            imagem.src = '../imagens/homem-criança.jpg'
+        } else if (idade < 21) {
+            imagem.src = '../imagens/homem-jovem.jpg'
+        } else if (idade < 50) {
+            imagem.src = '../imagens/homem-adulto.jpg'
+        } else {
+            imagem.src = '../imagens/homem-idoso.jpg'
+        }
+
+    } else {
+        if (fsex[1].checked) {
+
+            genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                imagem.src = '../imagens/mulher-criança.jpg'
+            } else if (idade < 21) {
+                imagem.src = '../imagens/mulher-jovem.jpg'
+            } else if (idade < 50) {
+                imagem.src = '../imagens/mulher-adulta.jpg'
+            } else {
+                imagem.src = '../imagens/mulher-idosa.jpg'
+            }
+        }
+    }
+
+res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+
+} 
+
 }
