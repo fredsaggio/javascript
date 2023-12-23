352da8a28e326caac38daf76bcd1fe2fd679ee68
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2023-12-23 18:57:03 -0300

    att
    att

---
+++ aula14ex/ex002/script.js
@@ -0,0 +1,14 @@
+function tabuada() {
+    let tabuada = document.getElementById('itabuada')
+    let seltabu = document.getElementById('iseltabu')
+
+    if (tabuada.value.length == 0) {
+        window.alert('Por favor, digite um número.')
+        
+    } else {
+    let valuetabuada = Number(tabuada.value)
+
+    }
+}
+
+
