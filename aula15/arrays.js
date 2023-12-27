37094b7e347632920fb3f90f7b0cc083d3bba043
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2023-12-27 14:53:25 -0300

    att

---
+++ aula15/arrays.js
@@ -0,0 +1,8 @@
+let num = [2, 1, 3, 4, 5]
+num[5] = 6
+num.push(8)
+num.push(0)
+num.sort()
+
+console.log(`Este é o nosso vetor ${num}`)
+console.log(`O valor 3 no nosso vetor é o ${num[3]}`)
---
+++ aula15/arrays2.js
@@ -0,0 +1,7 @@
+let valorarray = [2, 1, 3, 4, 5]
+
+valorarray.sort()
+
+for (let pos in valorarray) {
+    console.log(`${valorarray.} ${pos}`)
+}
