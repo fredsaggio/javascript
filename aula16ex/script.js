92f4e9ffde1689d1ba816e801009aec323771bde
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2023-12-31 12:34:25 -0300

    att
    att

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -2,30 +2,32 @@ function adicionar() {
     let numbervalue = document.getElementById('inumero')
 
     if (numbervalue.value.length == 0 || numbervalue.value < 1 || numbervalue.value > 100) {
-        window.alert('Valor inválido!')
+        alert('Valor inválido!')
 
     } else {
 
         let selectt = document.getElementById('itextonumero')
+        let teste4 = document.createElement('option')
+        let arrayvalores = []
         let teste2 = Number(numbervalue.value)
 
-        let teste3 = [teste2]
-        let teste4 = document.createElement('option')
 
-        teste4.text = `O valor ${teste3} foi adicionado.`
+        arrayvalores.push(teste2)
+
+        teste4.text = `O valor ${arrayvalores} foi adicionado.`
         selectt.appendChild(teste4)
-
+      
         }
     }
 
+    function finalizar() {
+        let sectionbox = document.getElementById('containerfirst')
+        let selectt = document.getElementById('itextonumero')
 
-function finalizar() {
-    let teste = document.getElementById('itextonumero')
+        if (selectt.options.length == 1) {
 
-    if (teste.value.length == 0) {
-        window.alert('Adicione um valor!')
+            alert('Adicione algum valor!')
 
-    } else {
-        for(let c = )
+        } else {
+        }
     }
-}
