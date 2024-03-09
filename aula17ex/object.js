aa33b929b321c553afd4de7cdb8ab3d1fd32014a
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-03-09 19:17:48 -0300

    att
    att

--- aula17ex/object.js
+++ aula17ex/object.js
@@ -1,14 +1,19 @@
-let amigo = {nome: 'josé',
-idade: 18,
-altura: 1.80,
-peso: 63.4,
-engordar(p){
-    this.peso += p 
-}}
+let amigo = {
+  nome: "josé",
+  idade: 18,
+  altura: 1.8,
+  peso: 63.4,
+  engordar(p) {
+    this.peso += p;
+  },
+};
 
-amigo.engordar(3)
+amigo.engordar(3);
 
-console.log(`O meu amigo se chama ${amigo.nome} e tem ${amigo.altura.toFixed(2)} de altura, além de ter ${amigo.idade} anos.`)
+console.log(
+  `O meu amigo se chama ${amigo.nome} e tem ${amigo.altura.toFixed(
+    2
+  )} de altura, além de ter ${amigo.idade} anos.`
+);
 
-console.log(`Ele também engordou ${amigo.peso} quilos.`)
-
+console.log(`Ele também engordou ${amigo.peso} quilos.`);
